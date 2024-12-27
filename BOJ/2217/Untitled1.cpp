0967eba02404e96fd2953d32fb2759cc8f6eae30
sabin <ghkfdhd@naver.com>
2024-12-27 20:26:45 +0900

    2217

--- BOJ/2217/Untitled1.cpp
+++ BOJ/2217/Untitled1.cpp
@@ -6,16 +6,10 @@ int main()
 	int n;
 	cin >> n;
 	int R[n];
-	int A[n];
 	for(int i=0;i<n;i++)
 	{
 		cin >> R[i];
 	}
 	sort(R, R+n);
-	for(int i=0;i<n;i++)
-	{
-		A[i]=R[i]*(n-i);
-	}
-	sort(A, A+n);
-	cout << A[n-1];
+	cout << R[0]*n;
 }
